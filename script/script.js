573b5f3dc5515edbb9c84307eed1da93c8a3dc87
C Wael <wael.charni@outlook.fr>
2024-03-18 18:33:31 +0100

    Refactored the decimalToBinary function to use a recursive approach. Added a showAnimation function for the number 5. Updated event handlers to accommodate these changes.

--- script/script.js
+++ script/script.js
@@ -3,45 +3,37 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
-
-  if (input === 0) {
-    result.innerText = "0";
-    return;
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
+};
 
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
-  }
-
-  console.log("Inputs: ", inputs);
-  console.log("Quotients: ", quotients);
-  console.log("Remainders: ", remainders);
-
-  result.innerText = remainders.reverse().join("");
+const showAnimation = () => {
 };
 
 const checkUserInput = () => {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt)) {
     alert("Please provide a decimal number");
     return;
   }
-  decimalToBinary(parseInt(numberInput.value));
+
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
-}
+};
 
 convertBtn.addEventListener("click", checkUserInput);
 
-numberInput.addEventListener("keydown", (e)=> {
+numberInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     checkUserInput();
   }
 });
-
